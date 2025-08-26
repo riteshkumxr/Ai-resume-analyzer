12a7a3a962ccc1de8ee61d9c83788afb034c3792
Ritesh Kumar <riteshkumxr3668@gmail.com>
2025-08-26 22:02:59 +0530

    Implement wipe functionality

--- app/routes/wipe.tsx
+++ app/routes/wipe.tsx
@@ -1,9 +1,64 @@
-// app/routes/wipe.tsx
-export default function Wipe() {
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router";
+import { usePuterStore } from "~/lib/puter";
+
+const WipeApp = () => {
+    const { auth, isLoading, error, clearError, fs, ai, kv } = usePuterStore();
+    const navigate = useNavigate();
+    const [files, setFiles] = useState<FSItem[]>([]);
+
+    const loadFiles = async () => {
+        const files = (await fs.readDir("./")) as FSItem[];
+        setFiles(files);
+    };
+
+    useEffect(() => {
+        loadFiles();
+    }, []);
+
+    useEffect(() => {
+        if (!isLoading && !auth.isAuthenticated) {
+            navigate("/auth?next=/wipe");
+        }
+    }, [isLoading]);
+
+    const handleDelete = async () => {
+        files.forEach(async (file) => {
+            await fs.delete(file.path);
+        });
+        await kv.flush();
+        loadFiles();
+    };
+
+    if (isLoading) {
+        return <div>Loading...</div>;
+    }
+
+    if (error) {
+        return <div>Error {error}</div>;
+    }
+
     return (
         <div>
-            <h1>Wipe Page</h1>
-            <p>This is the wipe page content.</p>
+            Authenticated as: {auth.user?.username}
+            <div>Existing files:</div>
+            <div className="flex flex-col gap-4">
+                {files.map((file) => (
+                    <div key={file.id} className="flex flex-row gap-4">
+                        <p>{file.name}</p>
+                    </div>
+                ))}
+            </div>
+            <div>
+                <button
+                    className="bg-blue-500 text-white px-4 py-2 rounded-md cursor-pointer"
+                    onClick={() => handleDelete()}
+                >
+                    Wipe App Data
+                </button>
+            </div>
         </div>
     );
-}
+};
+
+export default WipeApp;
