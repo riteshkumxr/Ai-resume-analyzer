ff1547324b7df5dd3da925eee3b0a3125caa541b
Ritesh kumar <164372473+riteshkumxr@users.noreply.github.com>
2025-08-29 09:14:52 +0530

    Update puter.ts

--- app/lib/puter.ts
+++ app/lib/puter.ts
@@ -350,8 +350,8 @@ export const usePuterStore = create<PuterStore>((set, get) => {
                     ],
                 },
             ],
-            { model: "claude-3-7-sonnet" }
-           // { model: "claude-sonnet-4" }
+            
+            { model: "claude-sonnet-4" }
         ) as Promise<AIResponse | undefined>;
     };
 
